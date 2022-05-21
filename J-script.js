d6632418b194b2daf69a57c39ed8c03a1b4dc4a0
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-05-20 20:58:11 -0400

    added complete nav/footer

--- J-script.js
+++ J-script.js
@@ -4,3 +4,4 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0] //FIRST E
 toggleButton.addEventListener('click', () => { //WHENEVER THIS ELEMENT IS CLICKED IT RUNS FUNCTION THAT SHOWS NAVBAR
     navbarLinks.classList.toggle('active') //ACCESS NAVBAR LINKS AND TOGGLES ALL CLASS IN LISTS
 })
+
